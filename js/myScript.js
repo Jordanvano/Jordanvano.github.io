b79c68bb6e4e14a98168d156e4b2872cfc2f47bc
Jordan <jordan@whoplusyou.com>
2022-06-01 08:44:44 -0400

    1/06/2022-4

--- js/myScript.js
+++ js/myScript.js
@@ -68,3 +68,13 @@
 
   f.addEventListener('submit', submitted);
 
+/*JS for "Try it" button to toggle between hiding and showing the DIV element*/
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
