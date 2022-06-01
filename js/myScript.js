b199a625f0895f7c8b375cdab7ce32c83b23fb59
Jordan <jordan@whoplusyou.com>
2022-06-01 08:02:16 -0400

    1/06/2022-2

---
+++ js/myScript.js
@@ -0,0 +1,16 @@
+/*Drag the image back and forth between the two div elements.*/
+<script>
+  function allowDrop(ev) (
+    ev.preventDefault();
+  )
+  
+  function drag(ev) (
+    ev.dataTransfer.setData("text", ev.target.id);
+  )
+  
+  function drop(ev) (
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  )
+  </script>
